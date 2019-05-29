7fcf4e541cf4113d3144a748180f8ea5222d471a
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-29 15:34:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,5 @@ end
 def printer(attendees)
     puts batch_badge_creator(attendees).pop
       badge_maker(attendees)
-      puts assign_rooms(attendees)
+      puts assign_rooms(attendees).pop
   end
