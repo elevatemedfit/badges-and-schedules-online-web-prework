7cdca61dd20d2088c515b2d3c9e11dc1ab7d27a5
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-29 12:15:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-   return room_assignments=attendees.each_with_index { |e,index|  "Hello, #{e}! You'll be assigned to room #{index+1}!"}
+   return room_assignments=attendees.map.each_with_index { |e,index|  "Hello, #{e}! You'll be assigned to room #{index+1}!"}
 end
 
 def printer(attendees)
